6b38c903a771c8c1eb8b673721ddf4579be4d4ab
langtaosha1990 <13301245282@163.com>
2024-04-22 15:38:41 +0800

    ww

---
+++ TestModel.swift
@@ -0,0 +1,14 @@
+//
+//  TestModel.swift
+//  UnityHelper
+//
+//  Created by Gpf 郭 on 2024/4/22.
+//
+
+import Foundation
+
+public class TestModel {
+    public func sayHai() {
+        print("hello world")
+    }
+}
