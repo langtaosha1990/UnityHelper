ce6d562a879d8d799fdbdfca41a31be859c4c532
langtaosha1990 <13301245282@163.com>
2024-04-22 16:05:05 +0800

    ff

--- Example/UnityHelper/GTestModel.swift
+++ Example/UnityHelper/GTestModel.swift
@@ -1,13 +1,13 @@
 //
-//  TestModel.swift
-//  UnityHelper
+//  GTestModel.swift
+//  UnityHelper_Example
 //
 //  Created by Gpf 郭 on 2024/4/22.
+//  Copyright © 2024 CocoaPods. All rights reserved.
 //
 
 import Foundation
-
-public class TestModel {
+public class GTestModel {
     public func sayHai() {
         print("hello world")
     }
